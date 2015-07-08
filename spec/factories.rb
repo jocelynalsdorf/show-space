17609022d631a81e3ef8196e1255f76e6c1be5a7
jocelynalsdorf <jfrodsla@gmail.com>
2015-07-08 12:30:13 -0700

    pass int specs for adding post

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,3 +1,8 @@
 FactoryGirl.define do
 
+factory(:post) do
+    title('fake post')
+    link('www.fake.com')
+    description('Some dumb text')
+  end
 end
---
+++ spec/features/add_new_post_pages_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe "the add a post process" do
+  it "adds a new post" do
+    post = FactoryGirl.build(:post)
+    visit root_path
+    click_link 'Add New Post'
+    fill_in 'Title', :with => post.title
+    fill_in 'Link', :with => post.link
+    fill_in 'Description', :with => post.description
+    click_on 'Create Post'
+    expect(page).to have_content post.title
+  end
+
+  it "gives an error when no data is entered into form fields" do
+    post = FactoryGirl.build(:post)
+    visit root_path
+    click_link 'Add New Post'
+    fill_in 'Title', :with => ''
+    fill_in 'Link', :with => ''
+    fill_in 'Description', :with => ''
+    click_on 'Create Post'
+    expect(page).to have_content 'errors'
+  end
+
+end
