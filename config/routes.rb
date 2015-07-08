f9fa13f15da199def02d3231005ca9d35429277d
jocelynalsdorf <jfrodsla@gmail.com>
2015-07-08 15:06:32 -0700

    add and setup post voting

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ Rails.application.routes.draw do
   
   devise_for :users
   resources :posts do
+  	member do
+  		get "like", to: "posts#upvote"
+  		get "dislike", to: "posts#downvote"
+  	end
   	resources :comments
   end
 
-
   root to: 'posts#index'
  
-
 end
